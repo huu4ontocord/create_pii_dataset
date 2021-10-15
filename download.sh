22e8c765c5c8c9c65058827ff21ea189f2262023
ontocord <ontocord@gmail.com>
2021-10-15 19:01:50 -0400

    Update download.sh

--- download.sh
+++ download.sh
@@ -3,4 +3,5 @@ python -m nltk.downloader punkt stopwords  wordnet
 python -m spacy download en_core_web_lg
 gdown https://drive.google.com/u/0/uc?id=1-9Wyu7ImEX8W21P701LeyTeIEFWpvixu&export=download
 gdown https://drive.google.com/u/0/uc?id=1-3066SYYuBE_d-zGkqpEX_reTlmYDo9Z&export=download
+gdown https://drive.google.com/uc?id=1okTZOKMX9DvA8-tjTZFk7WkhEr66E43o&export=download
 python create_pii_dataset/load.py
