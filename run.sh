4513e92562722589de1f3e4323969521dd6225af
ontocord <ontocord@gmail.com>
2021-10-15 01:41:02 -0400

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,5 @@
+python create_examples.py -target_lang en
+python create_examples.py -target_lang hi
+python create_examples.py -target_lang ar
+python create_examples.py -target_lang zh
+python create_examples.py -target_lang fa
